d57f89a54f60c4b364dbb195283678e5e83f06c8
Sagar Dafle <sagar.cdafle@gmail.com>
2016-07-02 04:38:14 -0700

    added toasts messages for signup fields

--- app/src/main/java/com/example/daffy/login_signupmodules_lab2_sagardafle/SignUpModuleFragment.java
+++ app/src/main/java/com/example/daffy/login_signupmodules_lab2_sagardafle/SignUpModuleFragment.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by daffy on 7/1/16.
@@ -43,13 +47,21 @@ public class SignUpModuleFragment extends Fragment {
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+        Log.d("HEREEEE", " ++++");
+              validatefields();
         UserData userdata = new UserData();
                 userdata.setFullname(fullName.getText().toString());
                 userdata.setEmailid(emailId.getText().toString());
                 userdata.setAge(Integer.parseInt(age.getText().toString()));
                 userdata.setMobile(mobileNumber.getText().toString());
                 userdata.setPassword(password.getText().toString());
+
+                Log.d("userdata.getFullname", userdata.getFullname());
+                Log.d("userdata.getEmailid", userdata.getEmailid());
+                Log.d("userdata.getAge", String.valueOf(userdata.getAge()));
+                Log.d("userdata.getMobile", userdata.getMobile());
+                Log.d("userdata.getPassword", userdata.getPassword());
+
 
 
                // helper.insertData(userdata);
@@ -72,6 +84,51 @@ public class SignUpModuleFragment extends Fragment {
         age = (EditText) signupview.findViewById(R.id.age);
         password = (EditText) signupview.findViewById(R.id.password);
         confirmPassword = (EditText) signupview.findViewById(R.id.confirmPassword);
+
+    }
+
+    // Check Validation Method
+    private void validatefields() {
+
+        // Get all edittext texts
+        String getFullName = fullName.getText().toString();
+        String getEmailId = emailId.getText().toString();
+        String getMobileNumber = mobileNumber.getText().toString();
+        String getAge = age.getText().toString();
+        String getPassword = password.getText().toString();
+        String getConfirmPassword = confirmPassword.getText().toString();
+
+        // Pattern match for email id
+        Pattern p = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b\"");
+        Matcher m = p.matcher(getEmailId);
+
+        // Check if all strings are null or not
+        if (getFullName.equals("") || getFullName.length() == 0
+                || getEmailId.equals("") || getEmailId.length() == 0
+                || getMobileNumber.equals("") || getMobileNumber.length() == 0
+                || getAge.equals("") || getAge.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0
+                || getConfirmPassword.equals("")
+                || getConfirmPassword.length() == 0)
+
+
+        Toast.makeText(getActivity(), "All fields are required.)",
+                Toast.LENGTH_LONG).show();
+
+            // Check if email id valid or not
+        else if (!m.find())
+            Toast.makeText(getActivity(), "Email ID is invalid.",
+                    Toast.LENGTH_LONG).show();
+
+            // Check if both password should be equal
+        else if (!getConfirmPassword.equals(getPassword))
+            Toast.makeText(getActivity(), "Passwords dont match!)",
+                    Toast.LENGTH_SHORT).show();
+
+            // Else do signup or do your stuff
+        else
+            Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
+                    .show();
 
     }
 }
